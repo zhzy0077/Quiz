2c68d15d6c5e090b9d0dc4a961970faadf8b7f63
Zheng <zhzy0077@hotmail.com>
2016-07-31 12:54:13 +0800

    add configuration for mime-mapping to xlsx

--- src/main/java/com/nuptsast/Application.java
+++ src/main/java/com/nuptsast/Application.java
@@ -2,6 +2,10 @@ package com.nuptsast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Zheng on 16/7/21.
@@ -11,5 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+}
+
+@Component
+class ServletCustomizer implements EmbeddedServletContainerCustomizer {
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        container.setMimeMappings(mappings);
     }
 }
